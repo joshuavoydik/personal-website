97056573e352027e837bc9a29398c7f62fac2515
Joshua <voydik@gmail.com>
2023-04-25 21:15:52 -0700

    PATHS

---
+++ build/precache-manifest.db98ae3c2d7ac860c9c997a1966251f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aa0444ff51dd5429d38",
+    "url": "/static/css/main.d7e8d7ca.chunk.css"
+  },
+  {
+    "revision": "5aa0444ff51dd5429d38",
+    "url": "/static/js/main.56323258.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d9c840d661ee00d7a34",
+    "url": "/static/js/2.7d1a5ed9.chunk.js"
+  },
+  {
+    "revision": "74d3b946e934f7fafcb969b3977915ef",
+    "url": "/index.html"
+  }
+];
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,6 @@ function Home() {
   return (
     <div>
       <Header />
-      <Hero />
       <Writing />
       <Newsletter />
       <Contact />
