890a942a3396504d50d7f0f7677be0a2f2150458
Joshua <voydik@gmail.com>
2023-04-25 20:44:25 -0700

    fuck

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
-import WritingPage from '.src//WritingPage';
-import NewsletterPage from '.src/NewsletterPage';
-import ContactPage from '.src/ContactPage';
+import WritingPage from './WritingPage';
+import NewsletterPage from './NewsletterPage'; // You've imported the NewsletterPage component here
+import ContactPage from './ContactPage';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/writing" component={WritingPage} />
-        <Route path="/newsletter" component={NewsletterPage} />
+        <Route path="/newsletter" component={NewsletterPage} /> // You've added the route for the NewsletterPage here
         <Route path="/contact" component={ContactPage} />
       </Switch>
     </Router>
@@ -19,3 +19,4 @@ function App() {
 }
 
 export default App;
+
